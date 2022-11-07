9ae5921dbbf9735bc6b3c422182657a13657aed4
nikhileshdhotre <nikhileshdhotre@gmail.com>
2022-11-07 13:05:55 +0530

    first commit to java project

---
+++ src/main/java/SEGROUP/MAVENJAVAPROJECTNIKHILESH/App.java
@@ -0,0 +1,13 @@
+package SEGROUP.MAVENJAVAPROJECTNIKHILESH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HELLO WORLD FROM D NIKHILESH" );
+    }
+}
